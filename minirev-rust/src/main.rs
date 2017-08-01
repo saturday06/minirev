8f183d4a4ce9cc84ac310c938c4ea93123d17791
Isamu Mogi <isamu@leafytree.jp>
2017-08-01 16:12:44 +0900

    wip

--- minirev-rust/src/main.rs
+++ minirev-rust/src/main.rs
@@ -1,85 +1,88 @@
 extern crate futures;
-extern crate tokio_core;
-extern crate rand;
 extern crate hyper;
-extern crate url;
+extern crate pretty_env_logger;
+extern crate tokio_core;
 
-use rand::Rng;
-use std::env;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use futures::{Future, Stream};
-use tokio_core::io::{copy, Io};
-use tokio_core::net::TcpListener;
+use futures::future::FutureResult;
+use hyper::{Get, StatusCode, Client};
+use hyper::server::{Http, Service, Request, Response};
+use std::string::String;
 use tokio_core::reactor::Core;
-use tokio_core::net::TcpStream;
+use futures::Future;
+use std::cell::{RefCell, RefMut};
+use std::ascii::AsciiExt;
+use futures::Stream;
+use futures::stream::Map;
+use hyper::Chunk;
+use hyper::header::ContentLength;
 
-static HOST: &'static str = "www.google.com";
+fn to_uppercase(chunk: Chunk) -> Chunk {
+    let uppered = chunk.iter()
+        .map(|byte| byte.to_ascii_uppercase())
+        .collect::<Vec<u8>>();
+    Chunk::from(uppered)
+}
 
-macro_rules! ret_err(
-	($e:expr) => {{
-		match $e {
-			Ok(v) => v,
-			Err(e) => { println!("Line {}: {}", line!(), e); return; }
-		}
-	}}
-);
-
-/// Given a `hyper::uri::RequestUri`, rewrite it to a `Url` substituting `HOST`
-/// for the domain.
-fn create_proxy_url(uri: hyper::uri::RequestUri, host: &str) -> Result<url::Url, url::ParseError> {
-    use hyper::uri::RequestUri::*;
-    match uri {
-        AbsolutePath(val) => url::Url::parse(&format!("http://{}{}", host, val)),
-        AbsoluteUri(_) => Err(url::Parse), //todo: rewrite uri
-        _ => Err(10abmw::InvalidScheme)
+fn handle(mut core: RefMut<Core>, req: Request) -> Response {
+    let client = Client::new(&core.handle());
+    match ("http://127.0.0.1:8080/".to_owned() + req.path()).parse() {
+        Ok(uri) => core.run(client.get(uri).map(|upstream_res| {
+            let headers = upstream_res.headers().clone();
+            Response::new()
+                // .with_headers(headers)
+                .with_status(upstream_res.status())
+                .with_body(upstream_res.body())
+        })).unwrap_or(Response::new().with_status(StatusCode::BadGateway)),
+        _ => Response::new().with_status(StatusCode::NotFound),
     }
 }
 
-//todo move mut to the type
-fn proxy_request(mut request: hyper::server::Request, host: &str) -> Result<hyper::client::Response, hyper::Error> {
-    use hyper::header::Host;
-
-    let mut client = hyper::Client::new();
-
-    // Read in the request body.
-    let mut request_body: Vec<u8> = Vec::new();
-    try!(::std::io::copy(&mut request, &mut request_body));
-
-    // The host header must be changed for compatibility with v-hosts.
-    let mut headers = request.headers;
-    headers.set(Host {
-        hostname: host.to_string(),
-        port: None
-    });
-
-    // Rewrite the target url from the client's request.
-    let url = try!(create_proxy_url(request.uri, host));
-
-    // Build and send the proxy's request.
-    let proxy_response = try!(
-        client.request(request.method, url)
-            .headers(headers)
-            .body(&request_body[..])
-            .send());
-
-    return Ok(proxy_response);
+struct Proxy {
 }
 
-fn handler(request: hyper::server::Request, mut response: hyper::server::Response<hyper::net::Fresh>) -> () {
-    let mut proxy_response = ret_err!(proxy_request(request, HOST));
+impl Service for Proxy {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = FutureResult<Response, hyper::Error>;
 
-    // Copy the proxy's response headers verbatim into the server's response
-    // headers.
-    *response.status_mut() = proxy_response.status.clone();
-    *response.headers_mut() = proxy_response.headers.clone();
+    fn call(&self, req: Request) -> Self::Future {
+        futures::future::ok(match req.method() {
+            &Get => {
+                thread_local!(static CORE_CELL: RefCell<Core> = RefCell::new(Core::new().unwrap()));
+                CORE_CELL.with(|core_cell| handle(core_cell.borrow_mut(), req))
+            },
+            _ => Response::new().with_status(StatusCode::NotFound),
+        })
+    }
+}
 
-    // Write the headers and rewrite the proxy's response body to the client.
-    let mut response = ret_err!(response.start());
-    ret_err!(::std::io::copy(&mut proxy_response, &mut response));
-    ret_err!(response.end());
+fn start() -> Result<i32, String> {
+    pretty_env_logger::init().map_err(|e|
+        e.to_string()
+    )?;
+    let addr = "127.0.0.1:1337".parse().map_err(|e|
+        format!("failed to parse address: {}", e)
+    )?;
+    let handler = || {
+        Ok(Proxy{})
+    };
+    let server = Http::new().bind(&addr, handler).map_err(|e|
+        format!("server.bind(): {}", e)
+    )?;
+    let local_addr = server.local_addr().map_err(|e|
+        format!("server.local_addr(): {}", e)
+    )?;
+    println!("Listening on http://{} with 1 thread.", local_addr);
+    server.run().map_err(|e|
+        format!("server.run(): {}", e)
+    )?;
+    return Ok(0)
 }
 
 fn main() {
-    let server = hyper::Server::http(handler);
-    ret_err!(server.listen("127.0.0.1:3000"));
+    match start() {
+        Ok(n) => println!("ok: {}", n),
+        Err(err) => println!("err: {}", err),
+    }
 }
